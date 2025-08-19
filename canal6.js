f4f12def395928e308bf3fb6f420909b076a47d3
TonnyMCH <90722910+TonnyMCH@users.noreply.github.com>
2025-08-18 22:11:34 -0600

    Update canal6.js

--- canal6.js
+++ canal6.js
@@ -1,7 +1,7 @@
 // Escuchar el evento keydown para capturar cuando se presiona una tecla
 document.addEventListener('ChannelUp', function(event) {
     // Verificar si la tecla presionada es "6"
-    if (event.key === '6') {
+    if (event.key === 'ChannelUp') {
         // Abrir el archivo Repretel.html en la misma ventana
         window.location.href = 'Canal 6.html';
 
